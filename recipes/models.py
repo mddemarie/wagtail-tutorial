c974586452f767e1e0d018154347a864cef5848b
Marie <mddemarie@gmail.com>
2018-06-19 17:28:12 +0200

    added a new recipe model and hmtl file

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,9 @@
+from django.db import models
+
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.search import index
 
 
 class RecipesIndexPage(Page):
@@ -8,4 +11,21 @@ class RecipesIndexPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname='full')
+    ]
+
+
+class RecipesPage(Page):
+    date = models.DateField('Recipe date')
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body', classname='full')
     ]
